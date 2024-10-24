3a2bc78f28a38466cd2a38ca8e8e03d79f00a4f6
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-10-24 19:46:50 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,36 @@
+
 import { createPreset, presets } from 'fumadocs-ui/tailwind-plugin';
+
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './content/**/*.{md,mdx}',
-    './mdx-components.{ts,tsx}',
+    './content/**/*.{mdx,tsx}',
     './node_modules/fumadocs-ui/dist/**/*.js',
+    './node_modules/fumadocs-openapi/dist/**/*.js',
   ],
-  // presets: [
-  //   createPreset({
-  //     preset: 'ocean',
-  //   }),
-  // ],
-  
   presets: [
-    createPreset(
-      {
-        addGlobalColors: true,
-        preset: {
-          ...presets.default,
-          dark: {
-            ...presets.default.dark,
-            background: '0 0% 2%',
-            foreground: '0 0% 98%',
-            popover: '0 0% 4%',
-            card: '0 0% 4%',
-            muted: '0 0% 8%',
-            border: '0 0% 14%',
-            accent: '0 0% 15%',
-            'accent-foreground': '0 0% 100%',
-            'muted-foreground': '0 0% 60%',
-          },
+    createPreset({
+      addGlobalColors: true,
+      preset: {
+        ...presets.default,
+        dark: {
+          ...presets.default.dark,
+          background: '0 0% 2%',
+          foreground: '0 0% 98%',
+          popover: '0 0% 4%',
+          card: '0 0% 4%',
+          muted: '0 0% 8%',
+          border: '0 0% 14%',
+          accent: '0 0% 15%',
+          'accent-foreground': '0 0% 100%',
+          'muted-foreground': '0 0% 60%',
         },
-      }
-    ),
+      },
+    }),
   ],
-
   theme: {
     extend: {
       fontFamily: {
@@ -67,5 +60,5 @@ export default {
       },
     },
   },
-};
 
+}
