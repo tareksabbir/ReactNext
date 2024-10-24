68861b8d33d75f2a24fc8b937b2c475d6fbc4920
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-10-24 19:46:15 +0600

    Update page.tsx

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -21,11 +21,21 @@ export default async function Page({
 
   return (
     <>
-      <div className="absolute inset-0 z-[-1] select-none overflow-hidden opacity-30">
+      {/* <div className="absolute inset-0 z-[-1] select-none overflow-hidden opacity-30">
         <div className="relative size-full">
           <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
         </div>
-      </div>
+      </div> */}
+      {/* <div className="absolute inset-0 z-[-1] overflow-hidden duration-1000 animate-in fade-in [perspective:2000px]">
+        <div
+          className="absolute bottom-[20%] left-1/2 size-[1200px] origin-bottom bg-fd-primary/30 opacity-30"
+          style={{
+            transform: 'rotateX(75deg) translate(-50%, 400px)',
+            backgroundImage:
+              'radial-gradient(50% 50% at center,transparent,hsl(var(--background))), repeating-linear-gradient(to right,hsl(var(--primary)),hsl(var(--primary)) 1px,transparent 2px,transparent 100px), repeating-linear-gradient(to bottom,hsl(var(--primary)),hsl(var(--primary)) 2px,transparent 3px,transparent 100px)',
+          }}
+        />
+      </div> */}
 
       <DocsPage toc={page.data.toc} full={page.data.full}>
         <DocsTitle>{page.data.title}</DocsTitle>
@@ -51,3 +61,4 @@ export function generateMetadata({ params }: { params: { slug?: string[] } }) {
     description: page.data.description,
   } satisfies Metadata;
 }
+
